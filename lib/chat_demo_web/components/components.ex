03f1b9655167be62643259f67d0a81c4edd5c6a3
Luís Simas <luissimas@protonmail.com>
2023-11-28 16:21:07 -0300

    fix: format message date in private function

--- lib/chat_demo_web/components/components.ex
+++ lib/chat_demo_web/components/components.ex
@@ -20,16 +20,6 @@ defmodule ChatDemoWeb.Components do
   end
 
   def message(assigns) do
-    # NOTE: we certainly shouldn't convert the date to user's local timezone
-    # on the server side, but this will serve our demo purposes
-    time =
-      assigns.message.inserted_at
-      |> Timex.Timezone.convert("UTC")
-      |> Timex.Timezone.convert("America/Sao_Paulo")
-      |> Timex.format!("%d/%m/%Y %H:%M", :strftime)
-
-    assign(assigns, time: time)
-
     ~H"""
     <div class="flex flex-col mb-2">
       <div class="flex items-center">
@@ -38,7 +28,7 @@ defmodule ChatDemoWeb.Components do
           <div class="flex items-center text-gray-900">
             <div>
               <p class="font-semibold"><%= @message.user.name %></p>
-              <time class="text-gray-500 text-sm"><%= @time %></time>
+              <time class="text-gray-500 text-sm"><%= format_date(@message.inserted_at) %></time>
             </div>
           </div>
         </div>
@@ -48,5 +38,14 @@ defmodule ChatDemoWeb.Components do
       </div>
     </div>
     """
+  end
+
+  defp format_date(time) do
+    # NOTE: we certainly shouldn't convert the date to user's local timezone
+    # on the server side, but this will serve our demo purposes
+    time
+    |> Timex.Timezone.convert("UTC")
+    |> Timex.Timezone.convert("America/Sao_Paulo")
+    |> Timex.format!("%d/%m/%Y %H:%M", :strftime)
   end
 end
