61e19f52327c69c955c4d3e64a416189b16b427f
Luís Simas <luissimas@protonmail.com>
2023-11-28 07:47:44 -0300

    fix: assign message time to socket

--- lib/chat_demo_web/components/components.ex
+++ lib/chat_demo_web/components/components.ex
@@ -20,12 +20,15 @@ defmodule ChatDemoWeb.Components do
   end
 
   def message(assigns) do
-    # TODO: parse timezone on client-side
+    # NOTE: we certainly shouldn't convert the date to user's local timezone
+    # on the server side, but this will serve our demo purposes
     time =
       assigns.message.inserted_at
       |> Timex.Timezone.convert("UTC")
       |> Timex.Timezone.convert("America/Sao_Paulo")
       |> Timex.format!("%d/%m/%Y %H:%M", :strftime)
+
+    assign(assigns, time: time)
 
     ~H"""
     <div class="flex flex-col mb-2">
@@ -35,7 +38,7 @@ defmodule ChatDemoWeb.Components do
           <div class="flex items-center text-gray-900">
             <div>
               <p class="font-semibold"><%= @message.user.name %></p>
-              <time class="text-gray-500 text-sm"><%= time %></time>
+              <time class="text-gray-500 text-sm"><%= @time %></time>
             </div>
           </div>
         </div>
