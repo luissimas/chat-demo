9908a761bf97d6e74f83a60b726d485197ddbba4
Luís Simas <luissimas@protonmail.com>
2023-12-07 12:17:24 -0300

    feat: add user presence tracking

---
+++ lib/chat_demo_web/channels/room_presence.ex
@@ -0,0 +1,11 @@
+defmodule ChatDemoWeb.RoomPresence do
+  @moduledoc """
+  Provides presence tracking to channels and processes.
+
+  See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
+  docs for more details.
+  """
+  use Phoenix.Presence,
+    otp_app: :chat_demo,
+    pubsub_server: ChatDemo.PubSub
+end
